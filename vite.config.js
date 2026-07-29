10f24383e92637331bb8e2335044af7fda4ba9df
William Coral <williamcoralts@gmail.com>
2026-07-29 11:58:13 -0500

    ci: agregar GitHub Actions deploy + base para Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/salchimonster/',   // necesario para GitHub Pages
   server: { port: 5173 },
 })
